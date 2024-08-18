0be018722e66103c720616ba3b6e4462bef53c0c
Oleh Kozlovskiy <oleg.kozlovskiy.35p@gmail.com>
2024-08-18 13:26:23 +0300

    Add project files.

---
+++ PractiseInitDelegate/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PractiseInitDelegate;
+
+namespace InitDelegate
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] names = { "Oleh", "Maria", "Ivan", "Yura", "Andrew", "Bim", "Toxic", "Dina" };
+            BubbleSort(names, new StringsLenghtComparer());
+            BubbleSort(names, new StringsComparer());
+        }
+
+        public static void BubbleSort(string[] arr, IComparer<string> comparer)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+                for (int j = 0; j < n - i - 1; j++)
+                    if (comparer.Compare(arr[j], arr[j + 1])>0)
+                    { 
+                        string temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+        }
+    }
+}
